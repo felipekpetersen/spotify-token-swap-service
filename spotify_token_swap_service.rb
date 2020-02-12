d4e4790e28c9e547cabf22d6b0bff8695aff3f07
Felipe Petersen <felipekpetersen@gmail.com>
2020-02-12 18:38:46 -0300

    mano sou burro

--- spotify_token_swap_service.rb
+++ spotify_token_swap_service.rb
@@ -275,14 +275,14 @@ AUTH_HEADER = "Basic " + Base64.strict_encode64(CLIENT_ID + ":" + CLIENT_SECRET)
   
   end
 
-  put 'v1/me/player/play' do
+  put '/v1/me/player/play' do
 
     # Request a new access token using the POST:ed refresh token
   
     http = Net::HTTP.new(SPOTIFY_API_ENDPOINT.host, SPOTIFY_API_ENDPOINT.port)
     http.use_ssl = true
   
-    request = Net::HTTP::put.new("v1/me/player/play")
+    request = Net::HTTP::get.new("/v1/me/player/play")
     auth = "Bearer " + params[:auth]
     request.add_field("Authorization", auth)
     
