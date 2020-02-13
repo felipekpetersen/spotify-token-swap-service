1164e481877a9279ff41317cf596cce1c1d067a9
Felipe Petersen <felipekpetersen@gmail.com>
2020-02-12 22:08:13 -0300

    change

--- spotify_token_swap_service.rb
+++ spotify_token_swap_service.rb
@@ -340,20 +340,18 @@ put '/v1/me/player/play' do
   request = Net::HTTP::Put.new("/v1/me/player/play")
   auth = "Bearer " + params[:auth]
   request.add_field("Authorization", auth)
-  request.add_field("Accept: application/json", "Content-Type: application/json")
-  context_uri = params[:context_uri]
-  request.add_field("context_uri", "spotify:track:1301WleyT98MSxVHPZCA6M")
+  request["Content-Type"] = "application/json"
+  # context_uri = params[:context_uri]
+  # request.add_field("context_uri", "spotify:track:1301WleyT98MSxVHPZCA6M")
   # encrypted_token = params[:refresh_token]
   # refresh_token = encrypted_token.decrypt(:symmetric, :password => ENCRYPTION_SECRET)
   # refresh_token = params[:refresh_token]
-  request.body = {
-    "context_uri" => "spotify:track:1301WleyT98MSxVHPZCA6M"
-  }
-  request.form_data = {
+  request.set_form_data({"context_uri" => "spotify:track:1301WleyT98MSxVHPZCA6M"})
+  # request.form_data = {
       # "grant_type" => "refresh_token",
       # "refresh_token" => refresh_token
       # "context_uri" => "spotify:track:1301WleyT98MSxVHPZCA6M"
-  }
+  # }
 
   response = http.request(request)
 
